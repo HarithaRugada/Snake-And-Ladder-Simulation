03b35d7693e85980c048f6f09e05187c77100480
HarithaRugada <haritharugada@gmail.com>
2020-06-10 11:03:56 +0530

    ADDED : Function to Roll Dice

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -3,3 +3,11 @@
 echo "WELCOME TO SNAKE AND LADDER SIMULATION"
 
 START=0
+
+function rollDice()
+{
+	faceValue=$(( $RANDOM%6 ))
+	echo "The obtained value on the dice is > " $(( $faceValue+1 ))
+}
+
+rollDice
